4842f9b807849d087936c8af3fc5a432da2dd251
Kim Min <kimmin17@sogang.ac.kr>
2022-11-29 23:10:42 +0900

    update

---
+++ source/Diarization.py
@@ -0,0 +1,12 @@
+from pyannote.audio import Pipeline
+from pydub import AudioSegment
+
+def seperation(wav):
+    pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token="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")
+    diarization = pipeline(wav)
+    song = AudioSegment.from_wav(wav)
+
+    for turn, _, speaker in diarization.itertracks(yield_label=True):
+        print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
+        temp_song = song[turn.start* 1000:turn.end* 1000]
+        temp_song.export("./sep/"+speaker+_+".wav", format="wav")
